788b2c7e53ea936082422062f70bbe736065b6f7
Shmulik <syraices@gmail.com>
2019-08-19 20:21:04 +0000

    CarrierWave

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ source 'https://rails-assets.org' do
 end
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
+gem 'figaro'
+gem 'fog-aws'
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -13,7 +13,7 @@ class GramsController < ApplicationController
   end
 
   def index
-
+    @grams = Gram.all
   end
 
   def new
@@ -62,7 +62,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found
